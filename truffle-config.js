a860e2c468663ea88f191d87a16d6f2afcf1517f
Pedro Gomes <pedrogomes94@gmail.com>
2019-12-26 18:17:42 +0000

    comment out bakaoh

--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,6 @@
 const hdWallet = require("./utils/hd-wallet");
-const bakaoh = require("./utils/bakaoh");
 const aquiladev = require("./utils/aquiladev");
+// const bakaoh = require("./utils/bakaoh");
 
 const chainId = 4;
 
@@ -13,20 +13,20 @@ module.exports = {
     },
     rinkeby: {
       provider: hdWallet.getProvider(chainId),
-      network_id: chainId // Rinkeby's id
-      // gas: 5500000, // Rinkeby has a lower block limit than mainnet
-      // gasPrice: 4000000000, // Setting gasPrice to 4 Gwei
-      // confirmations: 2, // # of confs to wait between deployments. (default: 0)
-      // timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
-      // skipDryRun: true // Skip dry run before migrations? (default: false for public nets )
-    },
-    bakaoh: {
-      provider: bakaoh.getProvider(chainId),
-      network_id: chainId // Rinkeby's id
+      network_id: chainId, // Rinkeby's id
+      gas: 5500000, // Rinkeby has a lower block limit than mainnet
+      gasPrice: 4000000000, // Setting gasPrice to 4 Gwei
+      confirmations: 2, // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true // Skip dry run before migrations? (default: false for public nets )
     },
     aquiladev: {
       provider: aquiladev.getProvider(chainId),
       network_id: chainId // Rinkeby's id
     }
+    // bakaoh: {
+    //   provider: bakaoh.getProvider(chainId),
+    //   network_id: chainId // Rinkeby's id
+    // }
   }
 };
